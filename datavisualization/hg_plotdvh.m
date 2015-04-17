6aa81d8fb2d40fb4f6984da3b5b7735eb747d5e3
Hubert <hubert.gabrys@gmail.com>
2015-04-17 18:17:50 +0200

    added varargin to plotdvh

--- datavisualization/hg_plotdvh.m
+++ datavisualization/hg_plotdvh.m
@@ -1,10 +1,19 @@
-function hg_plotdvh( args, vals, linecolor, str, ax )
+function hg_plotdvh( args, vals, linecolor, varargin )
 
-plot(ax, args, vals, 'LineWidth', 2, 'Color', linecolor, 'LineStyle', ':');
-xlabel(ax, {'Dose [Gy]'});
-ylabel(ax, {'Relative Volume [%]'});
-title(ax, str);
-axis(ax, [0 70 0 100]);
-hold on;
+if ~isempty(varargin)
+    str = varargin{1};
+    ax = varargin{2};
+    plot(ax, args, vals, 'LineWidth', 2, 'Color', linecolor, 'LineStyle', '--');
+    xlabel(ax, {'Dose [Gy]'});
+    ylabel(ax, {'Relative Volume [%]'});
+    title(ax, str);
+    axis(ax, [0 70 0 100]);
+    hold on;
+else
+    plot(args, vals, 'LineWidth', 2, 'Color', linecolor, 'LineStyle', '--');
+    xlabel({'Dose [Gy]'});
+    ylabel({'Relative Volume [%]'});
+    axis([0 70 0 100]);
+end
 
 end
