97d9689a9334a38770cb241f1b2ddf7d61939015
hubertgabrys <hubert.gabrys@gmail.com>
2017-10-06 14:26:51 +0200

    Renamed load_cube() to crop_cube() and moved it into calc_features().

--- feature_extraction/calc_features.m
+++ feature_extraction/calc_features.m
@@ -26,13 +26,13 @@ end
 
 for i=1:length(strucnames)
     strucname = strucnames{i};
-	if verbose
+    if verbose
         progress_tool(i, length(strucnames));
     end
     
     % load structure dosecube
     dose_cube = tps_data.dose.cube;
-    struct_cube = load_cube(tps_data, strucname, 'dose' );
+    struct_cube = crop_cube(tps_data, strucname, 'dose' );
     struct_cube_msk = struct_cube>0;
     struct_indicator_msk = tps_data.structures.(strucname).indicator_mask;
     
@@ -96,6 +96,92 @@ end
 %% output
 output = final_features;
 
+end
+
+function [struct_cube, struct_xgv, struct_ygv, struct_zgv] = crop_cube( tps_data, strucname, cube_type )
+% crop_cube crops the volume by cutting out zero-valued voxels
+% surrounging a non-zero valued voxels.
+% if shift == 'zero' then x1gv, x2gv and x3gv will be shifted in a way that
+% they start at zero go in positive direction.
+%
+
+%% Load the cube
+cube = tps_data.(cube_type).cube;
+indmsk = tps_data.structures.(strucname).indicator_mask;
+% in case that there are voxels with 0 dose inside volume, change 0
+% to 0.00001 (or any small value)
+cube(cube == 0 & indmsk == 1) = 0.00001;
+if strcmp(cube_type,'dose')
+    struct_cube = cube .* double(indmsk);
+elseif strcmp(cube_type,'ct')
+    struct_cube = cube .* double(indmsk);
+end
+xgv = tps_data.(cube_type).xVec;
+ygv = tps_data.(cube_type).yVec;
+zgv = tps_data.(cube_type).zVec;
+
+%% Crop cube
+V = struct_cube;
+x1gv = xgv;
+x2gv = ygv;
+x3gv = zgv;
+
+if any(isnan(V(:)))
+    error('NaN values within the cube!');
+end
+x1gv_min = 0;
+x1gv_max = 0;
+x2gv_min = 0;
+x2gv_max = 0;
+x3gv_min = 0;
+x3gv_max = 0;
+for i=1:length(x1gv)
+    if sum(sum(V(i,:,:))) > 0
+        if x1gv_min==0
+            x1gv_min = i;
+        end
+        if i>x1gv_max
+            x1gv_max = i;
+        end
+    end
+end
+for i=1:length(x2gv)
+    if sum(sum(V(:,i,:))) > 0
+        if x2gv_min==0
+            x2gv_min = i;
+        end
+        if i>x2gv_max
+            x2gv_max = i;
+        end
+    end
+end
+for i=1:length(x3gv)
+    if sum(sum(V(:,:,i))) > 0
+        if x3gv_min==0
+            x3gv_min = i;
+        end
+        if i>x3gv_max
+            x3gv_max = i;
+        end
+    end
+end
+V = V(x1gv_min:x1gv_max, x2gv_min:x2gv_max, x3gv_min:x3gv_max);
+
+%% Crop grid vectors
+x1gv = x1gv(x1gv_min:x1gv_max);
+x2gv = x2gv(x2gv_min:x2gv_max);
+x3gv = x3gv(x3gv_min:x3gv_max);
+
+%% Transform grid vectors so they origin at 0
+x1gv = abs(x1gv-x1gv(1));
+x2gv = abs(x2gv-x2gv(1));
+x3gv = abs(x3gv-x3gv(1));
+
+%% prepare output
+struct_cube = V;
+struct_xgv = x1gv;
+struct_ygv = x2gv;
+struct_zgv = x3gv;
 end
 
 
