4fbf107f9df62e90e6e32fd2dfb4d0f85da3b4cb
hubertgabrys <hubert.gabrys@gmail.com>
2018-01-30 22:10:13 +0100

    Reduced display precision of the progress tool.

--- utils/progress_tool.m
+++ utils/progress_tool.m
@@ -1,8 +1,8 @@
 function progress_tool(currentIndex, totalNumberOfEvaluations)
  
 if (currentIndex > 1 && nargin < 3)
-  Length = numel(sprintf('%3.2f%%',(currentIndex-1)/totalNumberOfEvaluations*100));
+  Length = numel(sprintf('%3.1f%%',(currentIndex-1)/totalNumberOfEvaluations*100));
   fprintf(repmat('\b',1,Length));
 end
-fprintf('%3.2f%%',currentIndex/totalNumberOfEvaluations*100);
+fprintf('%3.1f%%',currentIndex/totalNumberOfEvaluations*100);
 end
